7e41d5113f4fc64f812a2cc5c0491b132f02319f
Andrei2146 <claudiudibla04@gmail.com>
2024-01-18 11:24:30 +0200

    Project files

---
+++ Assets/logicscript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class logicscript : MonoBehaviour
+{
+    public int playerScore;
+    public Text scoreText;
+    public GameObject gameoverOverScreen; 
+
+    [ContextMenu("Increase Score")]
+    public void addScore(int scoreToAdd)
+    {
+        playerScore = playerScore + scoreToAdd;
+        scoreText.text = playerScore.ToString(); 
+    }
+    public void restartGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void gameOver()
+    {
+        gameoverOverScreen.SetActive(true);
+    }
+}
